5f5d8eabbc232b952087fe226e2fc2d954cc5e94
N3g4tivo <tonino77@gmail.com>
2022-11-06 14:52:31 +0100

    First Commit

--- app/src/main/java/it/comania/testautenticazionefirebase/LoginFragment.java
+++ app/src/main/java/it/comania/testautenticazionefirebase/LoginFragment.java
@@ -1,11 +1,21 @@
 package it.comania.testautenticazionefirebase;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 
 import android.text.TextUtils;
 import android.util.Log;
@@ -20,6 +30,10 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 import it.comania.testautenticazionefirebase.databinding.FragmentLoginBinding;
 
 public class LoginFragment extends Fragment {
@@ -32,6 +46,14 @@ public class LoginFragment extends Fragment {
     private FirebaseAuth mAuth;
     // [END declare_auth]
 
+    // this is for Google Authentication
+    // See: https://developer.android.com/training/basics/intents/result
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(new FirebaseAuthUIActivityResultContract(), new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+        @Override
+        public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+            onSignInResult(result);
+        }
+    });
 
     public LoginFragment() {
         // Required empty public constructor
@@ -66,7 +88,7 @@ public class LoginFragment extends Fragment {
             }
         });
 
-        // Log in
+        // Log in email
         binding.loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -78,6 +100,14 @@ public class LoginFragment extends Fragment {
                 } else {
                     login(email, password);
                 }
+            }
+        });
+
+        // Log in Google
+        binding.signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createSignInIntent();
             }
         });
 
@@ -106,6 +136,41 @@ public class LoginFragment extends Fragment {
         });
 
     }
+
+    ////////////////////////////////////
+    /// START Google Authentication ////
+    ////////////////////////////////////
+    public void createSignInIntent() {
+        // [START auth_fui_create_intent]
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        // Create and launch sign-in intent
+        Intent signInIntent = AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build();
+        signInLauncher.launch(signInIntent);
+        // [END auth_fui_create_intent]
+    }
+
+    // [START auth_fui_result]
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Sign in success, update UI with the signed-in user's information
+            Log.d(TAG, "signInWithCredential:success");
+            FirebaseUser user = mAuth.getCurrentUser();
+            updateUI(user);
+            Navigation.findNavController(requireView()).navigateUp();
+        } else {
+            // If sign in fails, display a message to the user.
+            Log.w(TAG, "signInWithCredential:failure", Objects.requireNonNull(result.getIdpResponse()).getError());
+            updateUI(null);
+        }
+    }
+    // [END auth_fui_result]
+
+    ////////////////////////////////////
+    ///  END Google Authentication  ////
+    ////////////////////////////////////
 
     private void updateUI(FirebaseUser user) {
 
